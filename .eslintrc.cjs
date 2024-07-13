e9a82bf2aa7b251f7aea5ff1c4e5a8f214d5b4cf
sama1mansn <136932204+sama1mansn@users.noreply.github.com>
2024-07-13 17:23:17 +0330

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true, node: true },
+  env: { browser: true, es2020: true, node: false},
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
